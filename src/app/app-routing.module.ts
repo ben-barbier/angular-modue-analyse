a6895314f231596d511d08868972c928437b9ea3
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2020-10-14 22:07:16 +0200

    ✨ add 2 pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'page1', loadChildren: () => import('./pages/page1/page1.module').then(m => m.Page1Module)},
+    {path: 'page2', loadChildren: () => import('./pages/page2/page2.module').then(m => m.Page2Module)},
+    {path: '**', redirectTo: 'page1'},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
